7a50c69bc628948870880e610bd711bce4f97baf
Natalia Gonzalez <up230319@alumnos.upa.edu.mxgit config --global user.name up230319-cloudgit pushexit:exitr{gtk+3{rgg{rr{g{{rg{r{gr>
2026-02-22 20:06:38 -0600

    primer intento

--- src/Helper.elm
+++ src/Helper.elm
@@ -5,7 +5,45 @@ import Html.Attributes
 
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+
+
+headers : String -> Html.Html msg
+headers param =
+    Html.div []
+        [ Html.h1 [] [ Html.text param ]
+        , Html.h2 [] [ Html.text param ]
+        , Html.h3 [] [ Html.text param ]
+        , Html.h4 [] [ Html.text param ]
+        , Html.h5 [] [ Html.text param ]
+        , Html.h6 [] [ Html.text param ]
+        ]
+
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink url textContent =
+    Html.a
+        [ Html.Attributes.href url ]
+        [ Html.text textContent ]
