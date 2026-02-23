ff61cc1ba1d2725f3f7a1d71ce975a13b3bc9a2b
Natalia Gonzalez <up230319@alumnos.upa.edu.mxgit config --global user.name up230319-cloudgit pushexit:exitr{gtk+3{rgg{rr{g{{rg{r{gr>
2026-02-22 20:10:49 -0600

    formato

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,14 +4,9 @@ import Html
 import Html.Attributes
 
 
-
-
-
 joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
-
-
 
 
 isUpperChars : List Char -> List Bool
@@ -19,14 +14,9 @@ isUpperChars list =
     List.map Char.isUpper list
 
 
-
-
-
 evalChars : List Char -> (Char -> Bool) -> List Bool
 evalChars list funTrans =
     List.map funTrans list
-
-
 
 
 headers : String -> Html.Html msg
@@ -39,7 +29,6 @@ headers param =
         , Html.h5 [] [ Html.text param ]
         , Html.h6 [] [ Html.text param ]
         ]
-
 
 
 hyperlink : String -> String -> Html.Html msg
